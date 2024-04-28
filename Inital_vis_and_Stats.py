80cfb4ca0d3edb83e807d5bdb38d5d1ec0194554
amanyifru <amanyifru0@gmail.com>
2024-04-28 11:49:11 +0300

    Mod for vis and stats

--- Inital_vis_and_Stats.py
+++ Inital_vis_and_Stats.py
@@ -2,21 +2,16 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# Load your dataset
-data = pd.read_csv('/path/to/your/Modified_PSDH_Power_Supply_Evaluation.csv')
+data = pd.read_csv('Modified_PSDH_Power_Supply_Evaluation.csv')
 
-# Compute summary statistics for the numerical columns
 summary_stats = data.describe()
 print(summary_stats)
 
-# Create histograms for 'Duration (Hours)' and 'Number of Customers Affected'
 fig, ax = plt.subplots(2, 1, figsize=(10, 8))
 
-# Histogram for 'Duration (Hours)'
 sns.histplot(data['Duration (Hours)'], bins=30, kde=True, ax=ax[0])
 ax[0].set_title('Distribution of Outage Duration (Hours)')
 
-# Histogram for 'Number of Customers Affected'
 sns.histplot(data['Number of Customers Affected'], bins=30, kde=True, ax=ax[1])
 ax[1].set_title('Distribution of Number of Customers Affected')
 
